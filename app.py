d03d4867b71e36719e2b6ef4f3bec185ab46fa10
aryan-sathish <poneeno@gmail.com>
2025-11-08 07:57:47 -0800

    boo

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')  # Added @ symbol
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
